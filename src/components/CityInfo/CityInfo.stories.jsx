b1bcae715586704f58f9b82dac3a2ac375b68332
sgonzavar <sgonzalezvargas.7cc@gmail.com>
2022-01-05 23:43:35 -0500

    Feat: new elements for CityInfo

--- src/components/CityInfo/CityInfo.stories.jsx
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import {CityInfo} from './CityInfo'
+import CityInfo from './CityInfo'
+
+//font 
+import 'typeface-roboto'
 
 export default {
     title: 'CityInfo',
     component: CityInfo,
 };
 
-export const CityExample = () => (<CityInfo city={"Medellin"} country={"Colombia"}></CityInfo>)
+export const CityExample = () => <CityInfo city="Medellin" country="Colombia"></CityInfo>
 
